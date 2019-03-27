11d3f23b8d3c3bb3683f348e69763c22d8cf990d
Senn Pateet <senn.patteet@hotmail.com>
2019-03-27 09:23:42 +0100

    server connection

--- Views/users/registration.php
+++ Views/users/registration.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Registration'
+$title = 'Registration';
 
 ?>
 
@@ -19,23 +19,14 @@ $title = 'Registration'
 </style>
 <h1><?php echo $title?></h1>
 <form action="$_POST" method = 'POST' >
+
 First Name: <input type="text" name="fname" id="firstname"><br>
 Last Name: <input type="text" name="lname" id="lastname"><br>
 Email: <input type="text" name="email" id="email"><br>
 Password: <input type="text" name="password" id="password"><br>
 Password Confirmation: <input type="text" name="confPassword" id="confPassword"><br>
-Location: <input type="text" name="location" id="location"><br>
-Age: <input type="number" name="age" id="age"><br>
-Website: <input type="text" name="website" id="website"><br>
-BIO: <input type="text" name="bio" id="bio"><br>
-Gender: <input type="radio" value= 'female'name="gender" id="gender">Female
-<input type="radio" value= 'male' name="gender" id="gender">Male
-<input type="radio" value= 'other' name="gender" id="gender">Other <br>
-<input type="submit" name= 'Register' value='Register' id='register' >
-
-
 
 </form>
-    
+
 </body>
 </html>
